cfde4823ac8f7f7d1d57303dbaf4bf2ec1c23189
dOOmcLiF <abc.abcd.1910@mail.ru>
2024-06-12 19:52:43 +0700

    Добавлены новые поля вывода

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,7 @@
 #include <QTextBlock>
 #include <QDate>
 #include <QTextTableCell>
+#include <QFont>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -25,7 +26,7 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     createsOdf();
-    createsOdfWithTable();
+    //createsOdfWithTable();
 }
 
 void MainWindow::createsOdf(){
@@ -36,19 +37,22 @@ void MainWindow::createsOdf(){
     cursor.movePosition(QTextCursor::Start);
 
     QTextCharFormat textFormat;
-    textFormat.setFontPointSize(10);
+    textFormat.setFont(QFont("Times"));
 
     QTextCharFormat boldFormat;
-    boldFormat.setFontWeight(QFont::Bold);
+    boldFormat.setFont(QFont("Times New Roman", 15, QFont::Bold));
+
     //HEADERS
-    QTextCharFormat header1Format = boldFormat;
-    header1Format.setFontPointSize(12);
-    header1Format.setFontUnderline(true);
-    header1Format.setForeground(QBrush(QColor("#0000FF")));
+    QTextCharFormat header1Format = textFormat;
+    header1Format.setFont(QFont("Times"));
+    header1Format.setFontPointSize(14);
+    //header1Format.setFontUnderline(true);
+    header1Format.setForeground(QBrush(QColor(Qt::black)));
 
     QTextCharFormat titleFormat = boldFormat;
     titleFormat.setFontPointSize(20);
-    header1Format.setForeground(QBrush(QColor("#0000FF")));
+    //titleFormat.setFontUnderline(true);
+    titleFormat.setForeground(QBrush(QColor(Qt::black)));
 
     QTextBlockFormat titleBlockFormat = cursor.block().blockFormat();
     titleBlockFormat.setAlignment(Qt::AlignHCenter);
@@ -59,11 +63,21 @@ void MainWindow::createsOdf(){
     cursor.insertBlock();
     cursor.setBlockCharFormat(titleFormat);
     cursor.setBlockFormat(titleBlockFormat);
-    cursor.insertText(QObject::tr("Test !"));
+    cursor.insertText(QObject::tr("Заголовок файла ахахаххаха"));
     cursor.insertBlock(); //new line
 
     cursor.setBlockFormat(blockFormat);
-    cursor.insertText(QObject::tr("Header 1"), header1Format);
+    cursor.insertText(QObject::tr("Текст 1"), header1Format);
+    cursor.insertBlock();
+    cursor.insertText(QObject::tr("Фамилия:\t________________"), header1Format);
+    cursor.insertBlock();
+    cursor.insertText(QObject::tr("Имя:\t\t________________"), header1Format);
+    cursor.insertBlock();
+    cursor.insertText("Автор:\tЯ");
+    cursor.insertBlock();
+    cursor.insertText("Версия:\tЯ");
+    cursor.insertBlock();
+    cursor.insertText("Дата:\t" + QDate::currentDate().toString("dd.MM.yyyy"));
     cursor.insertBlock();
 
     cursor.movePosition(QTextCursor::End);
@@ -71,9 +85,6 @@ void MainWindow::createsOdf(){
     QTextDocumentWriter writer("test2.odt");
     writer.setFormat("odf");
     writer.write(doc);
-    QTextDocumentWriter writer2("test2.html");
-    writer2.setFormat("html");
-    writer2.write(doc);
 
     delete doc;
 }
